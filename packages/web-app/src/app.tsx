6237e574d1d2476f36b14c0e907f2fadbb47efa8
A. Matías Quezada <amatiasq@gmail.com>
2024-09-30 12:52:42 +0200

    Autoformatting

--- packages/web-app/src/app.tsx
+++ packages/web-app/src/app.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useRef, useState } from "react"
-import { io, Socket } from "socket.io-client"
-import { ClientToServerEvents, ServerToClientEvents  } from "ws-backend/types/socket"
-import { Vehicle } from "ws-backend/types/vehicle";
+import { useEffect, useRef, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from 'ws-backend/types/socket';
+import { Vehicle } from 'ws-backend/types/vehicle';
 
 function App() {
-  const socketClient = useRef(io('http://localhost:3000', {
-    autoConnect: false,
-  }) as Socket<ServerToClientEvents, ClientToServerEvents>);
+  const socketClient = useRef(
+    io('http://localhost:3000', {
+      autoConnect: false,
+    }) as Socket<ServerToClientEvents, ClientToServerEvents>
+  );
 
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
 
@@ -26,12 +31,8 @@ function App() {
           ];
         }
 
-        return [
-          ...prevVehicles,
-          vehicle,
-        ];
+        return [...prevVehicles, vehicle];
       });
-
     });
 
     socket?.on('vehicles', (vehicles) => {
@@ -43,17 +44,27 @@ function App() {
     return () => {
       socket?.off('vehicle');
       socket?.disconnect();
-    }
+    };
   }, []);
 
-  return <div>
-    <h1>Yego Tiny</h1>
-    <ul>
-      {vehicles.map((vehicle) => (
-        <li key={vehicle.id}>{vehicle.name + ' (' + vehicle.plate_number + ')' + ' - ' + vehicle.battery + '%'}</li>
-      ))}
-    </ul>
-  </div>
+  return (
+    <div>
+      <h1>Yego Tiny</h1>
+      <ul>
+        {vehicles.map((vehicle) => (
+          <li key={vehicle.id}>
+            {vehicle.name +
+              ' (' +
+              vehicle.plate_number +
+              ')' +
+              ' - ' +
+              vehicle.battery +
+              '%'}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
